c4be1a99ad58df85ace3626c044deacf53b585d9
Michael Forman <michael.forman@brightsparklabs.com>
2017-04-05 19:22:45 +1000

    GEN-128: updates the document control block so that the version identifier is now the result of git describe --dirty, with the hash in (), rather than just the hash

--- index.js
+++ index.js
@@ -27,23 +27,30 @@ module.exports = function() {
 Last Modified On:      %ci%n%n\
 Last Modified By:      %cn%n%n\
 Generated From:        <%%= file.relative %%>%n%n\
-Version Identifier:    %h%n%n\
+Version Identifier:    <%%= describe %%> (%h)%n%n\
 --------------------------------------------------------------------%n\
 %n\\newpage%n%n"
 
     var gitCommand = 'git --no-pager log -n1 --pretty=tformat:\'' +
                         headerFormat +'\'';
 
+    var gitDescribeCommand = 'git describe --dirty';
+
     return es.map(function(file, cb){
         var headerText = exec(gitCommand + ' ' + file.path).stdout;
         headerText = headerText || '';
+        var describe = exec(gitDescribeCommand).stdout;
+        describe = describe || '';
+        describe = describe.replace(/\s+$/g, ''); // trim any trailing newlines
         var data = {
-            file : file
+            file : file,
+            describe : describe
         };
         file.contents = Buffer.concat([
             new Buffer(gutil.template(headerText, data)),
             file.contents
         ]);
+
         cb(null, file);
     });
 };
